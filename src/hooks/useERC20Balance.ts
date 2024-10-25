f52288eed84c03ef3a3ad0fe3c7e6c67a27aa529
dewantara tirta <dewantara.tirta@gmail.com>
2024-10-26 00:02:47 +0700

    feat(refresh balance)

--- src/hooks/useERC20Balance.ts
+++ src/hooks/useERC20Balance.ts
@@ -9,28 +9,36 @@ export const useERC20Balance = (address: HexAddress, token: HexAddress) => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<Error | null>(null);
 
-    useEffect(() => {
-        const fetchBalance = async () => {
-            setLoading(true);
-            try {
-                const result = await readContract(config, {
-                    address: token,
-                    abi: mockERC20ABI,
-                    functionName: 'balanceOf',
-                    args: [address],
-                });
-                setBalance(result as bigint);
-            } catch (err: unknown) {
-                setError(err as Error);
-            } finally {
-                setLoading(false);
-            }
-        };
+    const fetchBalance = async () => {
+        console.log(`checking balance for ${address} on token ${token}`);
+        setLoading(true);
+        try {
+            const result = await readContract(config, {
+                address: token,
+                abi: mockERC20ABI,
+                functionName: 'balanceOf',
+                args: [address],
+            });
+            console.log('result', result);
+            setBalance(result as bigint);
+        } catch (err: unknown) {
+            setError(err as Error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
+    useEffect(() => {
         if (address) {
             fetchBalance();
         }
     }, [address, token]);
 
-    return { balance, loading, error };
+    useEffect(() => {
+        if(balance){
+            console.log('balance', balance);
+        }
+    }, [balance]);
+
+    return { balance, loading, error, refreshBalance: fetchBalance };
 };

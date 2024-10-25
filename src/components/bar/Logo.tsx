5b602cb847f4f1fde025ad60acceed99341cca8f
dewantara tirta <dewantara.tirta@gmail.com>
2024-10-26 01:20:34 +0700

    feat(icon): change icon, fix wd form

--- src/components/bar/Logo.tsx
+++ src/components/bar/Logo.tsx
@@ -2,9 +2,10 @@ export default function Logo() {
   return (
     <a
       href={"/"}
-      className="flex items-center justify-start gap-2"
+      className="flex items-center justify-start gap-2 shine"
     >
-      <img src="https://res.cloudinary.com/dutlw7bko/image/upload/v1729689743/jackramp/logo_jackramp_bympnb.jpg" alt="Logo" className="object-cover object-center rounded-full w-[60px] h-[60px]"/>
+      <img src="/jackramp-icon-small-gap.png" alt="Logo" className="object-cover object-center h-[60px]"/>
+      <img src="/jackramp-text.png" alt="Logo" className="object-cover max-w-32 pt-2 hidden md:block"/>
     </a>
   );
 }

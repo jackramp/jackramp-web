ed42faab0249020f0aa2f35a6f29607ade45b1f0
dewantara tirta <dewantara.tirta@gmail.com>
2024-10-27 00:44:39 +0700

    feat(globe)

--- src/pages/HomePage/Index.tsx
+++ src/pages/HomePage/Index.tsx
@@ -3,7 +3,7 @@ import Homepage from './_components/Homepage'
 
 export const HomePage = () => {
     return (
-        <div className="flex w-screen h-screen z-10">
+        <div className="flex w-screen min-h-screen overflow-y-scroll z-10">
             <Suspense fallback={<div>Loading...</div>}>
                 <Homepage />
             </Suspense>

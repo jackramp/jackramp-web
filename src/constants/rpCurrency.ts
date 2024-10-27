85dda10370b12425eb4e3ec075c03ec24f49a601
FjrREPO <jossfajar21@gmail.com>
2024-10-27 21:24:38 +0700

    change currency & setup vercel🔥

---
+++ src/constants/rpCurrency.ts
@@ -0,0 +1,14 @@
+export const rpCurrency = {
+    id: "m0-token-id",
+    symbol: "IDR",
+    name: "IDR",
+    network: "IDR",
+    network_symbol: "IDR",
+    chain_id: 2, 
+    is_maintenance: false,
+    is_token: true,
+    contract_address: "0xYOUR_CONTRACT_ADDRESS",
+    decimals: 18,
+    max_trade_decimals: 6,
+    image: "https://res.cloudinary.com/dutlw7bko/image/upload/v1730037908/jackramp/rp_nfmtkf.png" 
+};
